72849d580ef7906a7b4fd42764a32e86dd835b33
Kleber Sales <klebinho77@hotmail.com>
2025-07-06 09:02:14 +0100

    change languague and add if

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("hello world")
+import os
+
+current_language = "en-US"
+msg = "Hello, World!"
+
+if current_language == "it-IT":
+    print("Ciao, Mondo!")
+elif current_language == "en-US":
+    print("Hello, World!")
+else:
+    print(msg)
+print("Bye!")
